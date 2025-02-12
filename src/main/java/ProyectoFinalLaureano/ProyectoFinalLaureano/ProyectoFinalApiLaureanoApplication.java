452399c58c50f1a1e4bf9dc07d86983bc104fe8a
Laureano <laureanodesousa12@gmail.com>
2025-02-12 16:00:00 +0100

    ProyectoBaseSpringVacio

---
+++ src/main/java/ProyectoFinalLaureano/ProyectoFinalLaureano/ProyectoFinalApiLaureanoApplication.java
@@ -0,0 +1,13 @@
+package ProyectoFinalLaureano.ProyectoFinalLaureano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalApiLaureanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalApiLaureanoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ProyectoFinalLaureano/ProyectoFinalLaureano/ProyectoFinalApiLaureanoApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoFinalLaureano.ProyectoFinalLaureano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalApiLaureanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
